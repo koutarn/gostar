ba1148480fc2c24244af0ce5559ce61ddd2aa0e2
koutarn <fuse.kanrinin@gmail.com>
2024-01-17 00:38:36 +0900

    update

--- cmd/root.go
+++ cmd/root.go
@@ -4,24 +4,87 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
+func execInit(projectName string){
+
+	// go.modファイルがあれば処理終了
+	if _ , err := os.Stat("go.mod"); err == nil {
+		fmt.Println("exits go.mod")
+		return
+	}
+
+	// go mod initを実行
+	// 	ユーザー名、プロジェクト名をいれたら実行してくれる
+	fmt.Println("go mod initを実行します")
+	fmt.Print("usrename:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	userName := scanner.Text()
+
+	out,err := exec.Command("go mod init Github.com." + userName + "/" + projectName,"").Output()
+	fmt.Println(out)
+	if err != nil {
+		fmt.Println("Error:",err)
+	}
+}
+
+func Create(){
+
+	dir ,err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error:",err)
+	}
+
+	// 	プロジェクト名はフォルダ名から自動取得
+	// 	TODO:指定フラグも作る
+	projectName := filepath.Base(dir)
+
+	execInit(projectName)
+
+	
+
+	// cobla-cliをインストール
+	// exec.Command("").Run()
+	// if err != nil {
+	// 	fmt.Println("Error:",err)
+	// }
+
+	// cobla-initを実行
+	exec.Command("coble-cli init").Run()
+	if err != nil {
+		fmt.Println("Error:",err)
+	}
+
+	// cobla add versionを実行
+	exec.Command("cobla-cli add version").Run()
+	if err != nil {
+		fmt.Println("Error:",err)
+	}
+
+    // 	Makefileをコピー
+    // 	testフォルダを作成
+
+
+
+}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gostar",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "いい感じにCLI開発環境を作成します",
+	Long: `GoのCLI開発環境を作成します。`,
+	Run: func(cmd *cobra.Command, args []string) { 
+		// 作成
+		Create()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -34,14 +97,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gostar.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
